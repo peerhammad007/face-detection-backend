2a91cce887ae455fa7504ce4826b747f7608297c
peer hammad <peerhammad007@gmail.com>
2024-04-01 10:11:48 +0530

    changed cors origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin: 'https://face-detection-backend-08wo.onrender.com'
 }));
 app.use(cookieParser());
 
